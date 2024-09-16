f9b2952c592c10f1793466a63642f0b22c2a1872
Jiří Joska <cz.terror@gmail.com>
2024-09-16 21:53:26 +0200

    Přidejte soubory projektu.

---
+++ UPG_Cv/Cv1/Program.cs
@@ -0,0 +1,39 @@
+﻿int m;
+
+if(args.Length > 0)
+{
+	m = Convert.ToInt32(args[0]);
+}
+else
+{
+	Console.Write("Zadej sirku m: ");
+	m = Convert.ToInt32(Console.ReadLine());
+}
+
+int n = 5 * m / 8;
+
+Console.WriteLine($"Velikost m x n = {m} x {n}");
+
+
+for (int y = 0; y < n; y++)
+{
+	for (int x = 0; x < m; x++)
+	{
+		int krX = 7 * m / 16;
+		int krY = n / 2;
+		int r = m * 3 / 16;
+
+		int z = (x - krX) * (x - krX) + (y- krY) * (y - krY) - r*r;
+
+		if (z >= 0)
+		{
+			Console.Write("-");
+		}
+		else
+		{
+			Console.Write("+");
+		}
+		
+	}
+	Console.WriteLine();
+}
