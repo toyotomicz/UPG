07423cce7c774bd29226a2829b5c77b76be548b0
Jiří Joska <cz.terror@gmail.com>
2024-09-21 18:12:25 +0200

    mrizka + upraveni zarovnani

--- UPG_semestralka/UPG_semestralka/MainForm.cs
+++ UPG_semestralka/UPG_semestralka/MainForm.cs
@@ -4,41 +4,80 @@ namespace UPG_semestralka
 	{
 		private int scenario;
 
-
 		public MainForm()
 		{
 			InitializeComponent();
 			this.MinimumSize = new Size(800, 600);
+			this.Text = "Vizualizace elektrostatického pole, scénáø: " + Convert.ToString(scenario);
 		}
+
 		private void drawingPanel_Paint(object sender, PaintEventArgs e)
 		{
 			Graphics g = e.Graphics;
-			g.Clear(Color.WhiteSmoke);
 
-			int cx = Width / 2; //stred platna
+			int cx = Width / 2; // stred platna
 			int cy = Height / 2;
 
+			// Kreslí møížku
+			DrawGrid(g, cx, cy);
+
 			// Pøíklad bodového náboje
-			DrawCharge(g, new Point(cx / 2, cy), -1, true);  // záporný náboj 0,-1
-			DrawCharge(g, new Point(3 * cx / 2, cy), 2, false); // kladný náboj 0,1
+			int charge1 = -1;
+			int charge2 = 2;
+
+			// Kreslí náboje na odpovídajících pozicích a zarovná je na stejné úrovni na ose Y
+			DrawCharge(g, new Point(cx / 2, cy), charge1, false); // záporný náboj
+			DrawCharge(g, new Point(3 * cx / 2, cy), charge2, true); // kladný náboj
+
+			
 		}
+
+		private void DrawGrid(Graphics g, int cx, int cy)
+		{
+			Pen gridPen = new Pen(Color.LightGray, 1);
+
+			// Nastavení vzdálenosti mezi møížkami (napø. každých 50 pixelù)
+			int gridSpacing = 50;
+
+			// Kreslení svislých èar
+			for (int x = 0; x < Width; x += gridSpacing)
+			{
+				g.DrawLine(gridPen, x, 0, x, Height);
+			}
+
+			// Kreslení vodorovných èar
+			for (int y = 0; y < Height; y += gridSpacing)
+			{
+				g.DrawLine(gridPen, 0, y, Width, y);
+			}
+
+			// Zvýraznìní støedového bodu
+			g.DrawLine(new Pen(Color.Black, 2), cx, 0, cx, Height); // støedová vertikální èára
+			g.DrawLine(new Pen(Color.Black, 2), 0, cy, Width, cy); // støedová horizontální èára
+		}
+
 		private void DrawCharge(Graphics g, Point position, int charge, bool isPositive)
 		{
-			int size = Math.Abs(charge) * 75; // Velikost náboje úmìrná jeho hodnotì
-			Color chargeColor = isPositive ? Color.Blue : Color.Red;
+			int size = Math.Abs(charge) * 100; // Velikost náboje úmìrná jeho hodnotì
+			Color chargeColor = isPositive ? Color.Red : Color.Blue;
+
+			// Kreslení náboje (kruhu)
 			g.FillEllipse(new SolidBrush(chargeColor), position.X - size / 2, position.Y - size / 2, size, size);
-			g.DrawString($"{charge} C", new Font("Arial", 10), Brushes.Black, position.X + size / 2, position.Y - size / 2);
+
+			// Kreslení textu náboje (zarovnaného na støed kruhu)
+			string chargeText = $"{charge} C";
+			Font chargeFont = new Font("Arial", 20);
+
+			// Získání velikosti textu, aby mohl být zarovnán doprostøed
+			SizeF textSize = g.MeasureString(chargeText, chargeFont);
+
+			// Vykreslení textu pøesnì do støedu náboje
+			g.DrawString(chargeText, chargeFont, Brushes.Black, position.X - textSize.Width / 2, position.Y - textSize.Height / 2);
 		}
 
 		private void drawingPanel_Resize(object sender, EventArgs e)
 		{
 			this.drawingPanel.Invalidate();
 		}
-
-		//public static void Main(string[] args)
-		//{
-		//	int scenario = args.Length > 0 ? int.Parse(args[0]) : 0;
-		//	Application.Run(new MainForm(scenario));
-		//}
 	}
 }
